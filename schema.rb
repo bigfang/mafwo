37b137bceebea1061965cc44d6aa5c42f79ac0fb
bigfang <bitair@gmail.com>
2016-11-06 23:01:15 +0800

    修改schema

--- schema.rb
+++ schema.rb
@@ -11,13 +11,12 @@ DB.create_table :countries do
   String :name_cn, :size=>10
   String :name_en, :size=>100
   String :continent, :size=>5
-  TrueClass :is_hot
 end
 
 DB.create_table :cities do
   primary_key :id, :auto_increment=>false
   foreign_key :country_id, :countries, :null=>false
-  String :name_cn, :size=>10
+  String :name_cn, :size=>20
   String :name_en, :size=>100
   String :summary, :size=>500
   Integer :visited_num
